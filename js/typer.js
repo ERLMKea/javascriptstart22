869239d480197c81d109dba965c9891c66897e20
erlm <erlm@kea.dk>
2023-09-04 09:07:01 +0200

    Initial commit

---
+++ js/typer.js
@@ -0,0 +1,21 @@
+// der er 7 (3+2+2) primitive typer i JavaScript
+// Number String Boolean
+// Undefined Null
+// Symbol og BigInt .. dem bruger vi ikke
+
+//der er yderligere 2 typer
+// object function
+
+function out(any) {
+    console.log(any)
+}
+function outtype(any) {
+    console.log("type= " + typeof any)
+    console.log(any)
+}
+
+
+let b
+let arr = [5, "hej", 5.6, 'a', b, null, [1,2,3], {"navn": "erik"}, out]
+out(5)
+arr.forEach(outtype)
